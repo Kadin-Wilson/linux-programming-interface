d1beb5dc926928e9f222ad7437f70dd3bf014921
Kadin Wilson <kadin.t.wilson@gmail.com>
2025-10-08 22:59:44 -0700

    Improve mem_node placement logic
    nodes are placed correctly.
    still need to correct merging of contigous nodes.

--- allocator.c
+++ allocator.c
@@ -54,42 +54,8 @@ void deallocate(void *mem) {
 }
 
 void place_node(mem_node *n) {
-    if (free_list.next == NULL) {
-        free_list.next = n;
-        free_list_tail.prev = n;
-        n->prev = &free_list;
-        n->next = &free_list_tail;
-        return;
-    }
-
-    if ((char*)n + n->size < (char*)free_list.next) { // before head
-        n->next = free_list.next;
-        n->prev = &free_list;
-        free_list.next->prev = n;
-        free_list.next = n;
-        return;
-    }
-    if ((char*)n > (char*)free_list_tail.prev + free_list_tail.prev->size) { // after tail
-        free_list_tail.prev->next = n;
-        n->prev = free_list_tail.prev;
-        n->next = &free_list_tail;
-        free_list_tail.prev = n;
-        return;
-    }
-    if ((char*)n + n->size <= (char*)free_list.next + free_list.next->size) { // grow head
-        n->size += free_list.next->size;
-        n->next = free_list.next->next;
-        n->prev = &free_list;
-        free_list.next->next->prev = n;
-        free_list.next = n;
-        return;
-    }
-    if ((char*)n <= (char*)free_list_tail.prev + free_list_tail.prev->size) { // grow tail
-        free_list_tail.prev->size += n->size;
-        return;
-    }
-
-    for (mem_node *cn = free_list.next->next; cn != &free_list_tail; cn = cn->next) {
+    for (mem_node *cn = free_list.next; cn != &free_list_tail; cn = cn->next) {
+        // before cn
         if ((char*)n + n->size < (char*)cn) {
             n->next = cn;
             n->prev = cn->prev;
@@ -97,35 +63,45 @@ void place_node(mem_node *n) {
             cn->prev = n;
             return;
         }
+        // merge left
         if ((char*)n + n->size <= (char*)cn + cn->size) {
             n->next = cn->next;
             n->prev = cn->prev;
+            n->size += cn->size;
             cn->next->prev = n;
             cn->prev->next = n;
-            n->size += cn->size;
             return;
         }
+        // merge right
         if ((char*)n <= (char*)cn + cn->size) {
             cn->size += n->size;
             return;
         }
     }
 
-    assert(false); // should be unreachable
+    // add to end of list
+    free_list_tail.prev->next = n;
+    n->prev = free_list_tail.prev;
+    n->next = &free_list_tail;
+    free_list_tail.prev = n;
 }
 
 void *check_free_list(size_t size) {
     return NULL;
 }
 
-void print_free_list() {
+int sprint_free_list(char *s) {
+    int c = 0;
     for (mem_node *p = &free_list; p != NULL; p = p->next) {
-        printf(
-            "\t(%p, %4lld, %p)\n", p, 
+        c += sprintf(
+            s + c,
+            "\t(%p, %4lld, %p)\n",
+            p, 
             (long long)p->size, 
-            p + (long long)p->size
+            (char*)p + p->size
         );
     }
+    return c;
 }
 
 int main() {
@@ -139,16 +115,22 @@ int main() {
         }
     }
 
+    static char out[10 * 13 * 200];
+    char *s = out;
+
     int order[10] = {3, 7, 9, 0, 2, 1, 5, 8, 4, 6};
     for (int i = 0; i < 10; i++) {
-        printf(
+        s += sprintf(
+            s,
             "deallocate (%p, %lu):\n", 
             (mem_node*)mem[order[i]] - 1, 
             (order[i] + 1) * 100 + sizeof(mem_node)
         );
         deallocate(mem[order[i]]);
-        print_free_list();
+        s += sprint_free_list(s);
     }
+
+    printf("%s", out);
 
     return EXIT_SUCCESS;
 }
